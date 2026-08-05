50123748ced4901d5bd350f55b27c1b106c436fd
Wasikul Fahim <wasikulfahim@Wasikuls-Mac-mini.local>
2026-08-06 01:53:58 +0600

    Indexer fixed

--- myDatasets/indexer.py
+++ myDatasets/indexer.py
@@ -9,25 +9,8 @@ import csv
 
 
 CLASS_TO_LABEL = {
-    "Back_Lateral_Change": 0,
-    "Car_InOut": 1,
-    "Collapse_Into_Chair": 2,
-    "Gentle_Jump": 3,
-    "Jogging_Fast": 4,
-    "Jogging_Slow": 5,
-    "Lying_Fast_SitAgain": 6,
-    "Lying_Slow_SitAgain": 7,
-    "Sit_HalfHeight_Fast": 8,
-    "Sit_HalfHeight_Slow": 9,
-    "Sit_LowHeight_Fast": 10,
-    "Sit_LowHeight_Slow": 11,
-    "Stairs_Fast": 12,
-    "Stairs_Slow": 13,
-    "Standing_KneeBend": 14,
-    "Standing_NoKneeBend": 15,
-    "Stumble_Walking": 16,
-    "Walking_Fast": 17,
-    "Walking_Slow": 18,
+    "Daily Living": 0,
+    "Fall": 1,
 }
 
 VALID_EXTENSIONS = {
@@ -47,23 +30,23 @@ def build_metadata(dataset_root, output_csv):
 
     rows = []
 
-    count = 0
-
     for img in dataset_root.rglob("*.png"):
-        count += 1
-
-        if count % 500 == 0:
-            print(f"Visited {count} images")
-
         parts = img.relative_to(dataset_root).parts
-    
-        if parts[0] != "Daily Living":
+
+        # Fall/SA21/image.png
+        if parts[0] == "Fall":
+            label = 1
+            activity = "Fall"
+            subject = parts[1]
+
+        # Daily Living/Walking_Slow/SA01/image.png
+        elif parts[0] == "Daily Living":
+            label = 0
+            activity = parts[1]
+            subject = parts[2]
+
+        else:
             continue
-
-        activity = parts[1]
-        subject = parts[2]
-
-        label = CLASS_TO_LABEL[activity]
 
         rows.append(
             {
@@ -73,9 +56,6 @@ def build_metadata(dataset_root, output_csv):
                 "subject": subject,
             }
         )
-
-    print("Visited total:", count)
-    print("Indexed:", len(rows))
 
     df = pd.DataFrame(rows)
 
