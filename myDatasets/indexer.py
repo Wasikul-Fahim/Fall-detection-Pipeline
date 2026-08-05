4de55f3b7b84d65076bfb8a0e82b10c631b8a5eb
Wasikul Fahim <wasikulfahim@Wasikuls-Mac-mini.local>
2026-08-06 00:51:20 +0600

    Indexer fix based on ADL

--- myDatasets/indexer.py
+++ myDatasets/indexer.py
@@ -9,8 +9,25 @@ import csv
 
 
 CLASS_TO_LABEL = {
-    "Daily Living": 0,
-    "Fall": 1,
+    "Back_Lateral_Change": 0,
+    "Car_InOut": 1,
+    "Collapse_Into_Chair": 2,
+    "Gentle_Jump": 3,
+    "Jogging_Fast": 4,
+    "Jogging_Slow": 5,
+    "Lying_Fast_SitAgain": 6,
+    "Lying_Slow_SitAgain": 7,
+    "Sit_HalfHeight_Fast": 8,
+    "Sit_HalfHeight_Slow": 9,
+    "Sit_LowHeight_Fast": 10,
+    "Sit_LowHeight_Slow": 11,
+    "Stairs_Fast": 12,
+    "Stairs_Slow": 13,
+    "Standing_KneeBend": 14,
+    "Standing_NoKneeBend": 15,
+    "Stumble_Walking": 16,
+    "Walking_Fast": 17,
+    "Walking_Slow": 18,
 }
 
 VALID_EXTENSIONS = {
